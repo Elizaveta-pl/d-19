76bad091eeb0219de2feb5c7e74b1a8e2a534bfb
npleshkov <npleshkov@gmail.com>
2019-10-04 21:48:57 +0400

    reverse

--- QT3/reverce.py
+++ QT3/reverce.py
@@ -1,10 +1,10 @@
-
 def reverse():
-    f_w= open("output.dat", 'wb')
-    for i in list(open("input.dat", mode = 'rb')):
-        print(i)
+    f_w = open("output.dat", 'wb')
+    for i in list(open("input.dat", mode='rb')):
+        i = i[:-1]
         g = i[::-1]
         f_w.write(g)
     f_w.close()
+
 
 reverse()
