7d2f017fc7e25a50052ace49e183746984a8d85b
npleshkov <npleshkov@gmail.com>
2019-10-15 22:13:38 +0400

    olimp

--- csv/olimp1.py
+++ csv/olimp1.py
@@ -7,5 +7,8 @@ with open('rez.csv', encoding="utf8") as csvfile:
     for row in list(reader)[1::]:
         row = str(row)[1:-2].split(',')
         if row[2].split('-')[2] == g and row[2].split('-')[3] == f:
-            i.append(f'{row[1].split()[3]} {row[-1][1:-1]}')
-i.sort(key=)
+            i.append((row[1].split()[3], row[-1][1:-1]))
+sorted(i, key=lambda si: si[0], reverse=True)
+sorted(i, key=lambda si: si[1])
+for j in i:
+    print(j[0], j[1])
