565201ff87ddb71d33429a9bb32b439e1efe2c1e
dj-job <npleshkov@gmail.com>
2019-11-09 10:21:41 +0400

    test3

--- test2.py
+++ test2.py
@@ -17,6 +17,11 @@ class Widget(QMainWindow):
         self.setWindowTitle('Table')
 
         self.table_widget = QTableWidget()
+        # Режим выделения. Выделяем только строки. Выделяем только одну строку.
+        self.table_widget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table_widget.setSelectionMode(QAbstractItemView.SingleSelection)
+        # блокируем редактирование таблицы
+        self.table_widget.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.setCentralWidget(self.table_widget)
 
     def fill(self):
@@ -55,14 +60,20 @@ class Widget(QMainWindow):
         #     row += 1
 
         with sqlite3.connect("baza.db") as connect:
-            for summa, whre, data, year in connect.execute("""SELECT * FROM Dohodi"""):
+            result1 = connect.cursor()
+            result = result1.execute("SELECT * FROM Dohodi")
+            # for row in result:
+            #     print(row)
+            #     # print("date:", row['date'])
+            # print(list(result))
+            for summ, operation, date, year in result:
                 row = self.table_widget.rowCount()
                 self.table_widget.setRowCount(row + 1)
-                self.table_widget.setItem(row, 0, QTableWidgetItem(str(summa)))
-                self.table_widget.setItem(row, 1, QTableWidgetItem(whre))
-                self.table_widget.setItem(row, 2, QTableWidgetItem(str(data)))
+                self.table_widget.setItem(row, 0, QTableWidgetItem(str(summ)))
+                self.table_widget.setItem(row, 1, QTableWidgetItem(str(operation)))
+                self.table_widget.setItem(row, 2, QTableWidgetItem(str(date)))
                 self.table_widget.setItem(row, 3, QTableWidgetItem(str(year)))
-                print(summa)
+
 if __name__ == '__main__':
     app = QApplication([])
 
