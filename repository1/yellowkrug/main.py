8969615fbb59c5b6ba56b0c675125d5aa2a8ed63
Elizaveta-pl <lisavet02@gmail.com>
2020-01-03 21:41:46 +0400

    yellow

--- repository1/yellowkrug/main.py
+++ repository1/yellowkrug/main.py
@@ -7,24 +7,36 @@ import sys
 
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
         uic.loadUi('Ui.ui', self)
         self.e = random.randint(0, 300)
-        self.pushButton.clicked.connect(self.fill)
+        self.draw = False
+        self.pushButton.clicked.connect(self.buttn)
 
-    def paintEvent(self, event):  # use to draw on the canvas
+    def paintEvent(self, event):                                 #use to draw on the canvas
         self.paint = QtGui.QPainter()
         self.paint.begin(self)
+        self.fill()
         self.paint.setRenderHint(QtGui.QPainter.Antialiasing)
         self.paint.setBrush(QtCore.Qt.white)
         self.paint.drawRect(event.rect())
+        self.paint.end()
+
 
     def fill(self):
-        self.paint.setPen(QtCore.Qt.yellow)
-        self.paint.setBrush(QtCore.Qt.yellow)
-        self.paint.drawEllipse(QtCore.QRect(self.e, self.e, self.e, self.e))
-        # self.paint.end()
+        if self.draw:
+            #self.paint.update()
+            self.paint.setPen(QtCore.Qt.yellow)
+            self.paint.setBrush(QtCore.Qt.yellow)
+            self.paint.drawEllipse(QtCore.QRect(self.e, self.e, self.e, self.e))
+            self.draw = False
+            #self.paint.update()
+        #
+
+    def buttn(self):
+        self.draw = True
 
 app = QApplication(sys.argv)
 ui = MainWindow()
