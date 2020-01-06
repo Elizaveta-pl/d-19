5cf1d895a2334b490691bc2fe5d2a399225de09c
Elizaveta-pl <lisavet02@gmail.com>
2020-01-06 20:24:23 +0400

    en

--- QT3/mysed.py
+++ QT3/mysed.py
@@ -1,14 +1,18 @@
-print('Введите иня текстового файла')
+print('Введите иня текстового файла:')
 n = input()
-text = list(open('input_test.txt'))
+text = list(open(f'{n}.txt'))
+print('')
+s_in = input()
+print('')
+s_out = input()
 # zamena = input().split(' ')
-zamena = '* s -- —'.split(' ')
+# zamena = '* s -- —'.split(' ')
 # print(f'zamena = {zamena}')
-#s_in = str(zamena[2])
-#s_out = str(zamena[3])
+# s_in = str(zamena[2])
+# s_out = str(zamena[3])
 
-s_in = '--'
-s_out = '-'
+# s_in = '--'
+# s_out = '-'
 
 g = open('output_tst.txt', "w")
 
