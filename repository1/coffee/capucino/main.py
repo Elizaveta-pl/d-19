e7d52a1f20970d7a8d9f0ac43df74bd392ed5027
npleshkov@gmail.com <Silver_2009>
2020-01-10 00:26:54 +0400

    capucino

--- repository1/coffee/capucino/main.py
+++ repository1/coffee/capucino/main.py
@@ -1,32 +1,95 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtGui, QtWidgets
 import sqlite3
 import sys
 
 
+PRODUCT_ID = ''
+EDIT_ROW = False
 
 class ClssDialog(QDialog):
     def __init__(self, parent=None):
         super(ClssDialog, self).__init__(parent)
-        uic.loadUi('addEditCoffeeForm_v1.ui', self)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.sql()
+
+        self.setupUi(self)
         self.pushButton_save.clicked.connect(
             self.save)  # Сохранить данные
+        self.pushButton_save.clicked.connect(
+            self.submitclose)  # Сохранить данные
+
+    def sql(self):
+        global PRODUCT_ID, ADD_ROW
+        if EDIT_ROW:
+            con = sqlite3.connect("coffee.db")
+            cur = con.cursor()
+            print(PRODUCT_ID)
+            text_sql = f'SELECT * FROM price  WHERE  id = {PRODUCT_ID}'
+            result = list(cur.execute(text_sql).fetchone())
+            self.label_ID.setText(str(result[0]))
+            self.lineEditName.setText(str(result[1]))
+            self.lineEditStepen.setText(str(result[2]))
+            self.lineEditTip.setText(str(result[3]))
+            self.lineEditOpisanie.setText(str(result[4]))
+            self.lineEditCena.setText(str(result[5]))
+            self.lineEditObem.setText(str(result[6]))
+
+
+    def setupUi(self, MainWindow):
+        pass
+        # sets up Submit button
+
+    def submitclose(self):
+        self.accept()
 
     def save(self):
-        print(3)
-    # def btnClosed(self):
-    #     self.close()
+        con = sqlite3.connect("coffee.db")
+        cur = con.cursor()
+        if EDIT_ROW:
+            con = sqlite3.connect("coffee.db")
+            cur = con.cursor()
+            id = self.label_ID.text()
+            name = self.lineEditName.text()
+            stepen = self.lineEditStepen.text()
+            tip = self.lineEditTip.text()
+            opisanie = self.lineEditOpisanie.text()
+            cena = self.lineEditCena.text()
+            obem = self.lineEditObem.text()
+            text_sql = f' UPDATE price SET Name = ?, Stepen = ?, Tip = ?, ' \
+                       f' Opisanie = ?,  Cena = ?,  Obem = ?  WHERE id = ?'
+            task = [name, stepen, tip, opisanie, cena, obem, id]
+            cur.execute(text_sql, task)
+        else:
+            con = sqlite3.connect("coffee.db")
+            cur = con.cursor()
+            name = self.lineEditName.text()
+            stepen = self.lineEditStepen.text()
+            tip = self.lineEditTip.text()
+            opisanie = self.lineEditOpisanie.text()
+            cena = self.lineEditCena.text()
+            obem = self.lineEditObem.text()
+            text_sql = f'INSERT INTO price(Name, Stepen, Tip, Opisanie,' \
+                       f' Cena, Obem) VALUES(?,?,?,?,?,?)'
+
+            task = [name, stepen, tip, opisanie, cena, obem]
+            cur.execute(text_sql, task)
+        con.commit()
+        con.close()
+        self.accept()
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
+        self.fill()
+
         self.pushButton.clicked.connect(self.fill)
         self.pushButton_add.clicked.connect(
             self.open_dialog)  # Открыть новую форму для добавления
         self.pushButton_edit.clicked.connect(
-            self.open_dialog)  # Открыть новую форму для редактирования
+            self.edit_row)  # Открыть новую форму для редактирования
 
 
     def fill(self):
@@ -35,7 +98,7 @@ class MainWindow(QMainWindow):
 
         labels = ['ID', 'Название сорта', 'Степень обжарки', 'Молотый/в зернах',
                   'Описание вкуса', 'Цена', 'Объем упаковки']
-
+        self.test6 = labels
         self.tableWidget.setColumnCount(len(labels))
         self.tableWidget.setHorizontalHeaderLabels(labels)
         with sqlite3.connect("coffee.db") as connect:
@@ -57,10 +120,24 @@ class MainWindow(QMainWindow):
         for d in range(self.tableWidget.rowCount()):
             self.tableWidget.removeRow(0)
 
+    def edit_row(self):
+        global PRODUCT_ID, EDIT_ROW
+        EDIT_ROW = True
+        PRODUCT_ID = (self.tableWidget.item(self.tableWidget.currentRow(),
+                                            0).text())
+        print(f'select_row = {PRODUCT_ID}')
+        if PRODUCT_ID != -1:
+            self.w = ClssDialog()
+            if self.w.exec_():
+                print(PRODUCT_ID)
+                self.fill()
 
     def open_dialog(self):
-        dialog = ClssDialog(self)
-        dialog.exec_()
+        global EDIT_ROW
+        EDIT_ROW = False
+        self.w = ClssDialog()
+        if self.w.exec_():
+            self.fill()
 
 
 app = QApplication(sys.argv)
