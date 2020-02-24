bef7f71406a7265acc1f18bd8bb82dc5cb41524e
dj-job <npleshkov@gmail.com>
2020-02-24 23:36:20 +0400

    yellow.py

--- pygame/yellow.py
+++ pygame/yellow.py
@@ -7,14 +7,29 @@ BLUE = (0, 0, 225)
 
 def draw(x_pos, clock):
     yellow = (255, 255, 0)
+    screen.fill((0, 0, 225))
+    pygame.display.flip()
 
     f = 0
     fps = 20
     v = 100
     for i in range(60):
-        pygame.draw.circle(screen, yellow, x_pos, f)
-        f += int(v * clock.tick(fps) / 1000)
-        pygame.display.flip()
+        mouse = 0
+        for event in pygame.event.get():
+            # при закрытии окна
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse = 1
+                draw(event.pos, clock)
+        if mouse == 0:
+            pygame.draw.circle(screen, yellow, x_pos, f)
+            f += int(v * clock.tick(fps) / 1000)
+            pygame.display.flip()
+        else:
+            break
+
+
 
 pygame.init()
 size = 600, 600
